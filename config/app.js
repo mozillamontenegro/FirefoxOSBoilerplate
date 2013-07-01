cad0b855c9e0e579f2852b183bf6954364c69639
Sreten Madzgalj <sretenmadzgalj@gmail.com>
2013-07-01 23:42:41 +0200

    Continued setting up

---
+++ config/app.js
@@ -0,0 +1,70 @@
+/*global angular */
+define(['angular'], function() {
+  // this is where our app definition is
+  var app = angular
+    .module('app')
+    .config(['$routeProvider', function($routeProvider) {
+      // here we specify routes
+      // which HTML and JS to execute when a certain route is requested
+      $routeProvider
+        .when('/', {
+          templateUrl: 'template/venom/list.html',
+          controller: 'ListCtrl'
+        })
+        .when('/category/:id', {
+          templateUrl: 'template/venom/category.html',
+          controller: 'CategoryCtrl'
+        })
+        .when('/food/:id', {
+          templateUrl: 'template/venom/food.html',
+          controller: 'FoodCtrl'
+        })
+        .when('/orders', {
+          templateUrl: 'template/venom/orders.html',
+          controller: 'OrdersCtrl'
+        })
+        .otherwise({
+          redirectTo: '/'
+        });
+    }]);
+
+//  app.controller('MainCtrl', ['$scope', '$navigate', '$location',
+//                            function($scope, $navigate, $location) {
+//    $scope.$navigate = $navigate;
+//    var search = $location.search();
+//    $navigate.go($location.path(), 'none').search(search);
+//  }]);
+
+  app.config(['$httpProvider', function($httpProvider) {
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  }]);
+
+  // TouchStart is faster than click, that's why we add this here as a
+  // directive. Use `ng-tap` in code rather than `ng-click`.
+  app.directive('ngTap', function() {
+    var isTouch = !!('ontouchstart' in window);
+    return function(scope, elm, attrs) {
+      // if there is no touch available, we'll fall back to click
+      if (isTouch) {
+        var tapping = false;
+        elm.bind('touchstart', function() {
+          tapping = true;
+        });
+        // prevent firing when someone is f.e. dragging
+        elm.bind('touchmove', function() {
+          tapping = false;
+        });
+        elm.bind('touchend', function() {
+          tapping && scope.$apply(attrs.ngTap);
+        });
+      }
+      else {
+        elm.bind('click', function() {
+          scope.$apply(attrs.ngTap);
+        });
+      }
+    };
+  });
+
+  return app;
+});
---
+++ config/config.js
@@ -0,0 +1,42 @@
+/*global angular */
+require.config({
+  shim: {
+    'angular': {
+      exports: 'angular'
+    }
+  },
+  paths: {
+    app: 'config/app',
+    angular: 'lib/angular'
+  },
+  baseUrl: '/'
+});
+
+(function() {
+  console.time('requirejs');
+  require([
+    // application
+    'app',
+
+    // dependencies
+    'angular'
+
+    // services
+//    'js/services/database.js',
+//    'js/services/dates.js',
+//    'js/services/http-cache.js',
+//    'js/services/shaking.js',
+
+    // controllers
+//    'js/controllers/list.js',
+//    'js/controllers/category.js',
+//    'js/controllers/food.js',
+//    'js/controllers/orders.js'
+    
+  ], function() {
+    console.timeEnd('requirejs');
+
+    angular.bootstrap(document, ['app']);
+  });
+
+})();
